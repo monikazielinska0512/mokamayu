581b39f5a946565e34e9f9ddc8450e829b25fc76
monikazielinska0512 <monika.zielinska0512@gmail.com>
2022-10-16 21:21:42 +0200

    clean up wardrobe_screen

--- lib/screens/wardrobe/wardrobe_screen.dart
+++ lib/screens/wardrobe/wardrobe_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mokamayu/reusable_widgets/appbar.dart';
-import '../../models/wardrobe/photo_item.dart';
+import 'package:mokamayu/reusable_widgets/photo_grid/photo_grid.dart';
+import '../../reusable_widgets/floating_button.dart';
 import '../../services/auth.dart';
 import 'clothes_add_screen.dart';
-import 'grid.dart';
 
 class WardrobeScreen extends StatefulWidget {
   const WardrobeScreen({Key? key}) : super(key: key);
@@ -13,73 +13,21 @@ class WardrobeScreen extends StatefulWidget {
 }
 
 class _WardrobeScreenState extends State<WardrobeScreen> {
-  final List<PhotoItem> _items = [
-    PhotoItem(
-        "https://images.pexels.com/photos/1772973/pexels-photo-1772973.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Stephan Seeber"),
-    PhotoItem(
-        "https://images.pexels.com/photos/1758531/pexels-photo-1758531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Liam Gant"),
-    PhotoItem(
-        "https://images.pexels.com/photos/1130847/pexels-photo-1130847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Stephan Seeber"),
-    PhotoItem(
-        "https://images.pexels.com/photos/45900/landscape-scotland-isle-of-skye-old-man-of-storr-45900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Pixabay"),
-    PhotoItem(
-        "https://images.pexels.com/photos/165779/pexels-photo-165779.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Scott Webb"),
-    PhotoItem(
-        "https://images.pexels.com/photos/548264/pexels-photo-548264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Krivec Ales"),
-    PhotoItem(
-        "https://images.pexels.com/photos/188973/matterhorn-alpine-zermatt-mountains-188973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Pixabay"),
-    PhotoItem(
-        "https://images.pexels.com/photos/795188/pexels-photo-795188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Melanie Wupper"),
-    PhotoItem(
-        "https://images.pexels.com/photos/5222/snow-mountains-forest-winter.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Jaymantri"),
-    PhotoItem(
-        "https://images.pexels.com/photos/789381/pexels-photo-789381.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Riciardus"),
-    PhotoItem(
-        "https://images.pexels.com/photos/326119/pexels-photo-326119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Pixabay"),
-    PhotoItem(
-        "https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Eberhard"),
-    PhotoItem(
-        "https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Mirsad Mujanovic"),
-    PhotoItem(
-        "https://images.pexels.com/photos/655676/pexels-photo-655676.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Vittorio Staffolani"),
-    PhotoItem(
-        "https://images.pexels.com/photos/592941/pexels-photo-592941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Tobi"),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: customAppBar(context, "Wardrobe Screen"),
-        body: Column(children: <Widget>[
-          Text("Jesteś zalogowany jako: " + AuthService().getCurrentUserUID()),
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const ClothesAddScreen()));
-            },
-            backgroundColor: Colors.green,
-            child: const Icon(Icons.add),
-          ),
-          Expanded(
-              child: Padding(
-                  padding: const EdgeInsets.all(20.0), child: photoGridView(_items)))
+        body: Stack(children: [
+          Column(children: [
+            Text(
+                "Jesteś zalogowany jako: " + AuthService().getCurrentUserUID()),
+            const Expanded(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 10.0),
+                    child: PhotoGrid()))
+          ]),
+          FloatingButton(
+              context, const AddClothesForm(), const Icon(Icons.add))
         ]));
   }
 }
