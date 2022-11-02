22afdd10b23f378486bc11d121e4b430d8c17892
Karina Szubert <karina1szubert@gmail.com>
2022-11-02 14:56:59 +0100

    small fixes

--- lib/screens/outfits/outfits_screen.dart
+++ lib/screens/outfits/outfits_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../reusable_widgets/photo_grid/photo_grid.dart';
 import '../../services/database/database_service.dart';
+import 'create_outfit_dialog.dart';
 
 class OutfitsScreen extends StatefulWidget {
   const OutfitsScreen({Key? key}) : super(key: key);
@@ -14,13 +15,29 @@ class _OutfitsScreenState extends State<OutfitsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      Column(children: [
-        Expanded(
-            child: Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 10.0),
-                child: PhotoGrid(stream: DatabaseService.readOutfits())))
+      body: Stack(children: [
+        Column(children: [
+          Expanded(
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 10.0),
+                  child: PhotoGrid(stream: DatabaseService.readOutfits())))
+        ]),
       ]),
-    ]));
+      floatingActionButton: checkForOutfitsScreen(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  FloatingActionButton? checkForOutfitsScreen() {
+    return FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return CustomDialogBox.outfitsDialog(context);
+              });
+        },
+        backgroundColor: Color.fromARGB(255, 244, 232, 217),
+        child: Icon(Icons.add));
   }
 }
