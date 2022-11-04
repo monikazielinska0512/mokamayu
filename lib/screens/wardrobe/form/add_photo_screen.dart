843367f7a865190dc037a3d97e20fc86eca8e352
monikazielinska0512 <monika.zielinska0512@gmail.com>
2022-11-04 23:35:13 +0100

    photo picker decoupled

--- lib/screens/wardrobe/form/add_photo_screen.dart
+++ lib/screens/wardrobe/form/add_photo_screen.dart
@@ -1,122 +1,48 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:mokamayu/reusable_widgets/basic_page.dart';
+import '../../../reusable_widgets/photo_picker.dart';
 import 'add_clothes_form.dart';
 
-class ImageUploads extends StatefulWidget {
-  const ImageUploads({Key? key}) : super(key: key);
+class PhotoPickerScreen extends StatefulWidget {
+  final PhotoPicker _picker = PhotoPicker(width: 370, height: 600);
 
-  @override
-  _ImageUploadsState createState() => _ImageUploadsState();
-}
 
-class _ImageUploadsState extends State<ImageUploads> {
-  File? _photo;
-  final ImagePicker _picker = ImagePicker();
+  PhotoPickerScreen({Key? key}) : super(key: key);
 
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Column(children: <Widget>[
-          const SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                _showPicker(context);
-              },
-              child: _photo != null
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.file(
-                        _photo!,
-                        width: 370,
-                        height: 650,
-                        fit: BoxFit.fill,
-                      ),
-                    )
-                  : Container(
-                      decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(20)),
-                      width: 370,
-                      height: 650,
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: Colors.grey[800],
-                      ),
-                    ),
-            ),
-          ),
-          Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ElevatedButton(
-                  onPressed: _photo != null
-                      ? () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      AddClothesForm(photo: _photo)));
-                        }
-                      : null,
-                  child: const Text("Przejdź dalej"))),
-        ]));
-  }
-
-  Future pickImage(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-    setState(() {
-      if (pickedFile != null) {
-        _photo = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  Future removeImage() async {
-    setState(() {
-      _photo = null;
-    });
-  }
-
-  void _showPicker(
-    context,
-  ) {
-    showModalBottomSheet(
+    return BasicPage(
         context: context,
-        builder: (BuildContext bc) {
-          return SafeArea(
-            child: Wrap(children: <Widget>[
-              _photo != null
-                  ? ListTile(
-                      leading: const Icon(Icons.photo_library),
-                      title: const Text('Remove'),
-                      onTap: () {
-                        removeImage();
-                        Navigator.of(context).pop();
-                      })
-                  : Container(),
-              ListTile(
-                  leading: const Icon(Icons.photo_library),
-                  title: const Text('Gallery'),
-                  onTap: () {
-                    pickImage(ImageSource.gallery);
-                    Navigator.of(context).pop();
-                  }),
-              ListTile(
-                leading: const Icon(Icons.photo_camera),
-                title: const Text('Camera'),
-                onTap: () {
-                  pickImage(ImageSource.camera);
-                  Navigator.of(context).pop();
-                },
-              ),
-            ]),
-          );
-        });
+        child: Center(
+            child: SizedBox(
+                height: double.maxFinite,
+                child: Column(children: <Widget>[
+                  _picker,
+                  TextButton(
+                      onPressed: () {
+                        print(_picker.photo);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => AddClothesForm(
+                                          photo: _picker.photo)));
+                            },
+                      child: Text("elo"))
+                ]))));
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
+// Padding(
+//     padding: const EdgeInsets.all(20.0),
+//     child: ElevatedButton(
+//         onPressed: () {
+// //           print(_picker.photo);
+// //         },
+//         child: const Text("Przejdź dalej"))),
+// // child: ElevatedButton(
+//     onPressed:
+// //     child: const Text("Przejdź dalej"))),
