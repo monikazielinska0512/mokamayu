cad68e9708695743419dbd81e858e739edc65c0c
Karina Szubert <karina1szubert@gmail.com>
2022-11-16 18:27:58 +0100

    add emulators config

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:mokamayu/constants/colors.dart';
 import 'package:mokamayu/services/authentication/auth.dart';
 import 'package:mokamayu/wrapper.dart';
-import 'models/user/firebase_user.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'generated/l10n.dart';
 import 'package:provider/provider.dart';
+
+import 'generated/l10n.dart';
+import 'models/user/firebase_user.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,9 +18,9 @@ void main() async {
 
   if (kDebugMode) {
     try {
-      //do testowania na emulatorach lokalnie - tu akurat moje ip, wiec jak cos to u siebie zmiencie
-      FirebaseFirestore.instance.useFirestoreEmulator('192.168.1.37', 8080);
-      await FirebaseAuth.instance.useAuthEmulator('192.168.1.37', 9099);
+      // do testowania na emulatorach lokalnie
+      FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('127.0.0.1', 9099);
     } catch (e) {
       // ignore: avoids_print
       print(e);
