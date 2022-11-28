5a92bbe47af8fb1771c4dc9485524950a2ed0bae
Justyna Frączek <justynkaf553@gmail.com>
2022-11-28 11:42:10 +0100

    edited outfits screen

--- lib/screens/outfits/outfits_add_screen.dart
+++ lib/screens/outfits/outfits_add_screen.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:mokamayu/models/models.dart';
 import 'package:mokamayu/widgets/widgets.dart';
 
+import '../../constants/constants.dart';
+
 class CreateOutfitPage extends StatelessWidget {
   CreateOutfitPage({Key? key, this.clothesList}) : super(key: key);
   Future<List<Clothes>>? clothesList;
@@ -12,14 +14,48 @@ class CreateOutfitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     map = Provider.of<PhotoTapped>(context, listen: true).getMap;
+    double deviceHeight(BuildContext context) =>
+        MediaQuery.of(context).size.height;
+    double deviceWidth(BuildContext context) =>
+        MediaQuery.of(context).size.width;
     return Scaffold(
       //appBar: customAppBar(context, "Create a look"),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: Text("Create outfit",
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              size: 35,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
       body: Column(
         children: <Widget>[
-          SizedBox(
-            height: 140,
-          ),
-          DragTargetContainer(map: map),
+          Stack(children: <Widget>[
+            Positioned(
+                child: Opacity(
+              opacity: 0.5,
+              child: Image.asset(
+                "assets/images/backgroundWaves.png",
+              ),
+            )),
+            Positioned(
+              child: Padding(
+                padding:
+                    EdgeInsets.fromLTRB(0, deviceHeight(context) * 0.14, 0, 0),
+                child: DragTargetContainer(map: map),
+              ),
+            )
+          ]),
+
           //categories for wardrobe
           //TODO
           //photos from wardrobe (button add if no photos) - scroll horizontally
