567c98745573e77838db105ae0b53210dd5c6e02
Justyna Frączek <justynkaf553@gmail.com>
2022-12-22 13:49:21 +0100

    uncomment emulators

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:mokamayu/constants/colors.dart';
@@ -20,16 +23,16 @@ void main() async {
   final appStateManager = AppStateManager();
   await appStateManager.initializeApp();
 
-  // if (kDebugMode) {
-  //   try {
-  //     // do testowania na emulatorach lokalnie
-  //     FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
-  //     await FirebaseAuth.instance.useAuthEmulator('127.0.0.1', 9099);
-  //   } catch (e) {
-  //     // ignore: avoids_print
-  //     print(e);
-  //   }
-  // }
+  if (kDebugMode) {
+    try {
+      // do testowania na emulatorach lokalnie
+      FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('127.0.0.1', 9099);
+    } catch (e) {
+      // ignore: avoids_print
+      print(e);
+    }
+  }
   runApp(MyApp(appStateManager: appStateManager));
 }
 
