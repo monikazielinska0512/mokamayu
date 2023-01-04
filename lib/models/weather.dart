7bb6e4f308e2019e66a9f175bc3e0d1d84ce1d43
Justyna Frączek <justynkaf553@gmail.com>
2023-01-04 17:24:40 +0100

    removed message

--- lib/models/weather.dart
+++ lib/models/weather.dart
@@ -50,16 +50,4 @@ class WeatherModel {
       return '🤷‍';
     }
   }
-
-  String getMessage(int temp) {
-    if (temp > 25) {
-      return 'It\'s 🍦 time';
-    } else if (temp > 20) {
-      return 'Time for shorts and 👕';
-    } else if (temp < 10) {
-      return 'You\'ll need 🧣 and 🧤';
-    } else {
-      return 'Bring a 🧥 just in case';
-    }
-  }
 }
